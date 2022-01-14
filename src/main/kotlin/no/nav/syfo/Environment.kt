a4e6ea6a2665e8573fbbb9679444eae128514f73
Andreas Nilsen <andreas.nilsen@nav.no>
2022-01-14 09:20:28 +0100

    remove onprem consumer (#9)
    * remove onprem consumer
    
    * set auto reset to none

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,7 +1,5 @@
 package no.nav.syfo
 
-import no.nav.syfo.kafka.KafkaConfig
-import no.nav.syfo.kafka.KafkaCredentials
 import no.nav.syfo.mq.MqConfig
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
@@ -11,25 +9,17 @@ data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val applicationName: String = getEnvVar("NAIS_APP_NAME", "syfoservice-mq-producer"),
     val syfoserviceQueueName: String = getEnvVar("MQ_SYFOSERVICE_QUEUE_NAME"),
-    val syfoserviceKafkaTopic: String = getEnvVar("SYFOSERVICE_KAFKA_TOPIC", "privat-syfo-syfoservice-mq"),
     override val mqHostname: String = getEnvVar("MQ_HOST_NAME"),
     override val mqPort: Int = getEnvVar("MQ_PORT").toInt(),
     override val mqGatewayName: String = getEnvVar("MQ_GATEWAY_NAME"),
     override val mqChannelName: String = getEnvVar("MQ_CHANNEL_NAME"),
-    override val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
-    override val cluster: String = getEnvVar("NAIS_CLUSTER_NAME"),
-    override val truststore: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
-    override val truststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD"),
     val syfoserviceKafkaTopicAiven: String = "teamsykmelding.syfoservice-mq"
-) : MqConfig, KafkaConfig
+) : MqConfig
 
 data class VaultSecrets(
     val serviceuserUsername: String = getFileAsString("/secrets/serviceuser/username"),
     val serviceuserPassword: String = getFileAsString("/secrets/serviceuser/password")
-) : KafkaCredentials {
-    override val kafkaUsername: String = serviceuserUsername
-    override val kafkaPassword: String = serviceuserPassword
-}
+)
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
     System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
